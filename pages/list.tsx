6e74d93eff1fcd0bcf2f8b6e1d27edf4ec9fadd7
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-12-19 12:04:14 +0800

    Update list.tsx

--- pages/list.tsx
+++ pages/list.tsx
@@ -30,52 +30,6 @@ const HomeRoutes: React.FC<HomeRouteProps> = ({ routes }) => {
 	return (
 		<>
 			<Page size="small">
-				<Text h1>
-					{' '}
-					Public Links{' '}
-					<Tooltip text={"What's this?"} placement="right">
-						<Link href="https://github.com/rishiosaur/ll">
-							<QuestionCircle />
-						</Link>
-					</Tooltip>
-				</Text>
-
-				<Text>
-					Welcome to LL! Here are some interesting links that this website's
-					owner's publicly shared (URL parameter forwarding <i>is</i>{' '}
-					available):
-				</Text>
-				<Table
-					data={routes.map((route) => ({
-						...route,
-						url: (
-							<Link color href={route.url}>
-								{route.url}
-							</Link>
-						),
-						name: (
-							<Link color href={route.name}>
-								{route.name}
-							</Link>
-						),
-					}))}>
-					<Table.Column prop="name" label="Route" />
-					<Table.Column prop="title" label="Title" />
-					{upMD && <Table.Column prop="description" label="Description" />}
-					<Table.Column prop="url" label="url" />
-				</Table>
-				<Spacer />
-
-				<Text small style={{ marginTop: '1rem' }}>
-					Proudly runnning [
-					<Link href="https://github.com/rishiosaur/ll">
-						<Code>ll</Code>
-					</Link>
-					] by{' '}
-					<Link href="https://rishi.cx" color>
-						Rishi Kothari
-					</Link>
-				</Text>
 			</Page>
 		</>
 	)
